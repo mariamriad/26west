c4519a00cf235e37a3a5098c037191b76c4b1380
Mariam <itsmariamriad@gmail.com>
2019-05-11 14:49:24 -0700

    Footer setup

--- footer.php
+++ footer.php
@@ -14,19 +14,23 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentysixwest' ) ); ?>">
+		<div class="col-1 contact-us">
+			<?php echo do_shortcode('[ninja_form id=1]'); ?>
+		</div>
+		<div class="col-2">
+			<div class="logo">
 				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'twentysixwest' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'twentysixwest' ), 'twentysixwest', '<a href="http://www.mariamriad.com">Mariam Riad</a>' );
-				?>
-		</div><!-- .site-info -->
+				the_custom_logo();
+				if ( is_front_page() && is_home() ) :
+					?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"></a></>
+				<?php endif; ?>
+			</div>
+			<div class="site-info">
+				<p class="privacy"><a href="/credits/">Privacy policy</a></p>
+				<p class="copy">&copy; <?php echo date('Y'); ?> Pezzente Holdings Inc. <a href="/credits/">Site credits.</a></p>
+			</div><!-- .site-info -->
+		</div><!-- .col-2 -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
